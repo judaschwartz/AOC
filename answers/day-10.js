5988c845893ca9b1da18ca7eaaa2f9c8ba00df9d
Yehuda Schwartz <judaschwartz@users.noreply.github.com>
2020-12-10 16:30:52 -0500

    Update day-10.js

--- answers/day-10.js
+++ answers/day-10.js
@@ -6,14 +6,16 @@ input.forEach ((n,ind) => {
     if (n - input[ind - 1] === 3){three++}
 })
 ans2=1
+function trib(n) {
+    if (n < 3) {return 0} 
+    if (n !== 3) {
+        return trib(n - 1) + trib(n - 2) + trib(n - 3)
+    } else {return 1}
+}
 input.forEach ((n,ind) => {
     if (n - input[ind - 1] === 1){cnt++}
     if (n - input[ind - 1] === 3){
-// this is a sub-optimal sulution, if I have time I will come back and work out the algorithm for a random run of ones any length
-        if (cnt === 2) {ans2 *= 2}
-        if (cnt === 3) {ans2 *= 4}
-        if (cnt === 4) {ans2 *= 7}
-        if (cnt === 5) {ans2 *= 13}
+        ans2 *= trib(cnt+3)
         cnt=0
     }
 })
