dd5937369e2e33c30c51b9ae58ee99dbd0b2caf6
Yehuda Schwartz <judaschwartz@users.noreply.github.com>
2020-12-10 17:09:50 -0500

    Update day-10.js

--- answers/day-10.js
+++ answers/day-10.js
@@ -6,12 +6,6 @@ input.forEach ((n,ind) => {
     if (n - input[ind - 1] === 3){three++}
 })
 ans2=1
-function trib(n) {
-    if (n < 3) {return 0} 
-    if (n !== 3) {
-        return trib(n - 1) + trib(n - 2) + trib(n - 3)
-    } else {return 1}
-}
 input.forEach ((n,ind) => {
     if (n - input[ind - 1] === 1){cnt++}
     if (n - input[ind - 1] === 3){
@@ -20,3 +14,10 @@ input.forEach ((n,ind) => {
     }
 })
 console.log(`Answer 1: ${one*(three)}, Answer 2: ${ans2}`)
+//this is a recursive function and will run out of resources in the browser once n >~30 (that shouldn't happen with the given inputs from AoC)
+function trib(n) { 
+    if (n < 3) {return 0} 
+    if (n !== 3) {
+        return trib(n - 1) + trib(n - 2) + trib(n - 3)
+    } else {return 1}
+}
