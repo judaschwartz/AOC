acd515179a1ad202cdb48ec86dad138e72acbde9
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2021-12-10 13:17:09 -0500

    day 9

--- answers-2021/day-06.js
+++ answers-2021/day-06.js
@@ -1,14 +1,11 @@
-input = document.querySelector('pre').innerText.trim().split`,`
-fish=input.reduce((a,n)=>{
-  a[n]++
-  return a  
-}, [0,0,0,0,0,0,0,0,0])
-for (i=80;i>0;i--) {
-  fish=[fish[1],fish[2],fish[3],fish[4],fish[5],fish[6],fish[0]+fish[7],fish[8],fish[0]]
-}
-ans1 = fish.reduce((a,n)=>{return a+n},0)
-for (i=256-80;i>0;i--) {
-  fish=[fish[1],fish[2],fish[3],fish[4],fish[5],fish[6],fish[0]+fish[7],fish[8],fish[0]]
-}
-ans2 = fish.reduce((a,n)=>{return a+n},0)
-console.log(`Answer 1: ${ans1}, Answer 2: ${ans2}`)
+input = document.querySelector('pre').innerText.trim().split`\n`.map(n=>[9,...n.split(''),9])
+ans1=0;
+p = [input[0].map(e=>9), ...input, input[0].map(e=>9)]
+input.map((r, ri)=>{
+  r.slice(1,-1).map((c, ci)=>{
+    if (p[ri+1][ci] >c && p[ri][ci+1] >c && p[ri+1][ci+2] >c && p[ri+2][ci+1] > c) {
+     ans1 += (c*1) +1
+    }
+  })
+})
+console.log(`Answer 1: ${ans1}, Answer 2: ...`)
