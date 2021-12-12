817c89fb31ddc1ac4908ead03a481706add0472c
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2021-12-12 02:15:43 -0500

    day 10

--- answers-2021/day-10.js
+++ answers-2021/day-10.js
@@ -1,71 +1,43 @@
 input = document.querySelector('pre').innerText.trim().split`\n`
-ans1 = input.map(r=>{
-open =[]
-  return r.split('').map(c=>{
-    if ('[(<{'.includes(c)) {
-      open.push(c)
-    } else {
-      if (c === '}') {
-        if (open.pop() !== '{') {
-          return 1197
-        }
-      } else if (c === ']') {
-        if (open.pop() !== '[') {
-          return 57
-        }
-      } else if (c === ')') {
-        if (open.pop() !== '(') {
-          return 3
-        }
-      } else if (c === '>') {
-        if (open.pop() !== '<') {
-          return 25137
-        }
-      }
-    }
-  }).filter(Boolean)
-}).filter(a=>a.length).reduce((a,r)=>{return a + r[0]}, 0)
-
-input = document.querySelector('pre:last-of-type').innerText.trim().split`\n`
-aa = input.map(r=>{
+ans = input.map(r=>{
   open =[]
   r.split('').map(c=>{
-    if (open && '[(<{'.includes(c)) {
+    if (Array.isArray(open) && '[(<{'.includes(c)) {
       open.push(c)
-    } else if (open) {
+    } else if (Array.isArray(open)) {
       if (c === '}') {
         if (open.pop() !== '{') {
-          open = false
+          open = 1197
         }
       } else if (c === ']') {
         if (open.pop() !== '[') {
-          open = false
+          open = 57
         }
       } else if (c === ')') {
         if (open.pop() !== '(') {
-          open = false
+          open = 3
         }
       } else if (c === '>') {
         if (open.pop() !== '<') {
-          open = false
+          open = 25137
         }
       }
     }
   })
-return open
-}).filter(Boolean)
-ans2= aa.map(r=>{
-   return r.reverse().reduce((a,c)=>{
-     if (c === '{') {
-        return (a*5)+3
-      } else if (c === '[') {
-        return (a*5)+2
-      } else if (c === '(') {
-        return (a*5)+1
-      } else if (c === '<') {
-        return (a*5)+4
-      }
-return a
-    }, 0)
-}).sort((a,b)=>{return a-b})[Math.floor(aa.length/2)]
-console.log(`Answer 1: ${ans1}, Answer 2: ${ans2}`)
+  return open
+})
+ans1 = ans.filter(o=>!Array.isArray(o)).reduce((a,r)=>{return a + r}, 0)
+ans2 = ans.filter(o=>Array.isArray(o)).map(r=>{
+  return r.reverse().reduce((a,c)=>{
+    if (c === '{') {
+      return (a*5)+3
+    } else if (c === '[') {
+      return (a*5)+2
+    } else if (c === '(') {
+      return (a*5)+1
+    } else if (c === '<') {
+      return (a*5)+4
+    }
+  }, 0)
+}).sort((a,b)=>{return a-b})
+console.log(`Answer 1: ${ans1}, Answer 2: ${ans2[Math.floor(ans2.length/2)]}`)
