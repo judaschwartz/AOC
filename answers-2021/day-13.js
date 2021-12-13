8607261e5b2ed235e29b5ab07ee8543038c8192c
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2021-12-13 09:51:56 -0500

    day 13

--- answers-2021/day-13.js
+++ answers-2021/day-13.js
@@ -4,4 +4,4 @@ ans1 = [...new Set(input[0].split`\n`.map(i=>{
   fold = input[1].split`\n`[0].split`=`[1]
   return (split[0] > fold*1 ? fold*2 - split[0] : split[0])+','+split[1]
 }))]
-console.log(`Answer 1: ${ans1}, Answer 2: ...`)
+console.log(`Answer 1: ${ans1.length}, Answer 2: ...`)
