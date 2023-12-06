10511bcb16f0284c339fdb6789afb075d1ce7cff
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2023-12-06 10:17:54 -0500

    2023 day 6

---
+++ answers-2023/day-06.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('pre').innerText.trim().split("\n").map(l => l.split(/ +/g).slice(1))
+const wins = (t, d) => {
+  for (let h = 0; h < t; h++) {
+    if (h * (t-h) > d)
+      return t - (h * 2) + 1
+  }
+}
+let ans1 = 1
+for (let i = 0; i < input[0].length; i++) {
+  ans1 *= wins(Number(input[0][i]), Number(input[1][i]))
+}
+console.log(`Answer 1: ${ans1}, Answer 2: ${wins(Number(input[0].join('')), Number(input[1].join('')))}`)
