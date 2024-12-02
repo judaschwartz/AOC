b1d3b6745aec8d258f17239e3ba9ac4dd0605a9f
Yehuda Schwartz <judaschwartz@users.noreply.github.com>
2024-12-01 20:47:20 -0500

    Update day-01.js

--- answers-2024/day-01.js
+++ answers-2024/day-01.js
@@ -2,6 +2,6 @@ const input = document.querySelector('pre').innerText.trim().split("\n")
 const r = input.map(p => Number(p.split('   ')[1])).sort()
 const l = input.map(p => Number(p.split('   ')[0])).sort()
 const ans1 = r.reduce((a, n, i) => a + Math.abs(n - l[i]), 0)
-const ans2 = l.reduce((a, n, i) => a + (n * r.filter(s => s === n).length), 0)
+const ans2 = l.reduce((a, n) => a + (n * r.filter(s => s === n).length), 0)
 
 console.log(`Answer 1: ${ans1}, Answer 2: ${ans2}`)
