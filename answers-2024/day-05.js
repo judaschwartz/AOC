67a41d9c6a41d290b47aaa46e27dbc5013f48ce4
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2024-12-05 14:59:42 -0500

    2024 day 5 refactor

--- answers-2024/day-05.js
+++ answers-2024/day-05.js
@@ -1,20 +1,13 @@
-const input = document.querySelector('pre').innerText.trim().split("\n\n")
-const m = {}
-input[0].split("\n").map(r => {
-  r = r.split('|')
-  if (!m[r[1]]) m[r[1]] = []
-  m[r[1]].push(r[0])
-})
-let ans1 = 0, ans2 = 0
-
-for (let p of input[1].split("\n").map(e => e.split(','))) {
-  let correct = true
-  for (i = 0; i < p.length; i++) {
-    const move = []
-    m[p[i]]?.map(n => p.slice(i).map((e, ind) => e === n && move.push(...p.splice(i + ind, 1))))
-    if (move.length) p.splice(i--, 0, ...move) && (correct = false)
+const [rules, input] = document.querySelector('pre').innerText.trim().split("\n\n").map(s => s.split("\n"))
+const m = rules.reduce((a, r) => ([x, y] = r.split('|'), (a[y] = a[y] || []).push(x), a), {})
+let ans1 = 0, ans2 = 0, t, oo = []
+for (let p of input.map(e => e.split(','))) {
+  let fix = false
+  for (j = 0; j < Math.ceil(p.length/2); j++) {
+    p.slice(j).map((e, i) => m[p[j]]?.includes(e) && oo.push(...p.splice(i + j, 1)))
+    if (oo.length) p.splice(j--, 0, ...oo) && (fix = true, oo = [])
   }
-  ans1 += p[Math.floor(p.length /2)] * correct
-  ans2 += p[Math.floor(p.length /2)] * !correct
+  t = p[Math.floor(p.length/2)], ans1 += t * !fix, ans2 += t * fix
 }
+
 console.log(`Answer 1: ${ans1}, Answer 2: ${ans2}`)
