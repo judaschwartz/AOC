cc70d6ad27d72eeb6cbf8b6fea54fc45ef1ff8d9
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2024-12-07 21:18:25 -0500

    2024 day 7 refactor

--- answers-2024/day-07.js
+++ answers-2024/day-07.js
@@ -2,25 +2,17 @@ let ans1 = 0, ans2 = 0
 document.querySelector('pre').innerText.trim().split("\n").map(l => {
     p = l.split(': ')
     nums = p[1].trim().split(' ').map(Number)
-    ans1 += !!either(nums[0], nums.slice(1), Number(p[0])) * p[0]
-    ans2 += !!tri(nums[0], nums.slice(1), Number(p[0])) * p[0]
+    ans1 += either(nums[0], nums.slice(1), Number(p[0])) * p[0]
+    ans2 += tri(nums[0], nums.slice(1), Number(p[0])) * p[0]
 }, 0)
 
 function either(a, b, ex) {
-  if (b.length === 1) {
-    if ((a * b[0] === ex) || (a + b[0] === ex)) return 1
-  } else if (a + b[0] <= ex) {
-    if (either(a * b[0], b.slice(1), ex) || either(a + b[0], b.slice(1), ex)) return 1
-  }
-  return 0
+  return (b.length === 1 && (a * b[0] === ex || a + b[0] === ex)) ||
+    (a + b[0] <= ex && (either(a * b[0], b.slice(1), ex) || either(a + b[0], b.slice(1), ex)))
 }
 function tri(a, b, ex) {
-  if (b.length === 1) {
-    if ((a * b[0] === ex) || (a + b[0] === ex) || (Number(String(a) + b[0])) === ex) return 1
-  } else if (a + b[0] <= ex) {
-    if (tri(Number(String(a) + b[0]), b.slice(1), ex) || tri(a * b[0], b.slice(1), ex) || tri(a + b[0], b.slice(1), ex)) return 1
-  }
-  return 0
+  return (b.length === 1 && ((a * b[0] === ex) || (a + b[0] === ex) || (Number(String(a) + b[0])) === ex)) ||
+    (a + b[0] <= ex && (tri(Number(String(a) + b[0]), b.slice(1), ex) || tri(a * b[0], b.slice(1), ex) || tri(a + b[0], b.slice(1), ex)))
 }
 
 console.log(`Answer 1: ${ans1}, Answer 2: ${ans2}`)
